7af4b7bb58f673a67db94e00641dcbac94d1560f
Rejinold Pearson <57714077+rejinold@users.noreply.github.com>
2021-06-15 19:36:07 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install golang
-GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
-go get -u github.com/tomnomnom/httprobe
-go get -u github.com/tomnomnom/anew
-go get github.com/tomnomnom/waybackurls
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
-unzip aquatone_linux_amd64_1.7.0.zip; sudo mv aquatone /usr/bin/
